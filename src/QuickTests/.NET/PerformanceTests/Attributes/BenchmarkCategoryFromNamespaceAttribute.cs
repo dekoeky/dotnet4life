fbe12099aa0aee927ccd678b8d7e8b0c8eb75b98
Wesley Van Koeckhoven <wvk@outlook.be>
2025-03-10 15:48:35 +0100

    attributes

---
+++ src/QuickTests/.NET/PerformanceTests/Attributes/BenchmarkCategoryFromNamespaceAttribute.cs
@@ -0,0 +1,7 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PerformanceTests.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+internal class BenchmarkCategoryFromNamespaceAttribute(Type benchmarkType)
+    : BenchmarkCategoryAttribute(benchmarkType.Namespace ?? throw new ArgumentNullException(nameof(benchmarkType)));
---
+++ src/QuickTests/.NET/PerformanceTests/Attributes/CustomCategoryDiscoverer.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Running;
+using System.Reflection;
+
+namespace PerformanceTests.Attributes;
+
+internal class CustomCategoryDiscoverer : DefaultCategoryDiscoverer
+{
+    public CustomCategoryDiscoverer()
+    {
+        Console.WriteLine($"Hello world from {nameof(CustomCategoryDiscoverer)}");
+    }
+    public override string[] GetCategories(MethodInfo method)
+    {
+        //Get the (final reflected) type to which this test belongs
+        var type = method.ReflectedType;
+
+        var categories = new List<string>();
+        categories.AddRange(base.GetCategories(method));
+        var x = GetNamespaceParts(type?.Namespace).ToArray();
+        categories.AddRange(x);
+        return categories.ToArray();
+    }
+
+    private static IEnumerable<string> GetNamespaceParts(string? ns)
+    {
+        while (!string.IsNullOrEmpty(ns))
+        {
+            yield return ns;
+            var lastDotIndex = ns.LastIndexOf('.');
+            if (lastDotIndex == -1) break; // No more dots, we're done
+
+            ns = ns[..lastDotIndex]; // Remove the last part
+        }
+    }
+}
