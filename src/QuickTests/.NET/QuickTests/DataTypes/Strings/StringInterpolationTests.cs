835d6fdea097b720f32b7ff1636049014fcfcd34
Wesley Van Koeckhoven <wvk@outlook.be>
2025-03-10 15:48:45 +0100

    string interpolation test

---
+++ src/QuickTests/.NET/QuickTests/DataTypes/Strings/StringInterpolationTests.cs
@@ -0,0 +1,19 @@
+﻿namespace QuickTests.DataTypes.Strings;
+
+[TestClass]
+public class StringInterpolationTests
+{
+    [TestMethod]
+    public void SimpleExample()
+    {
+        //Arrange
+        var now = DateTime.Now;
+
+        //Act
+        var result = $"Today is {now:O}";
+
+        //Assert
+        Console.WriteLine(result);
+        Assert.IsFalse(string.IsNullOrEmpty(result));
+    }
+}
