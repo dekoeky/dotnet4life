cf27fde365feffffa71f0076f93581909f6c5013
Wesley Van Koeckhoven <wvk@outlook.be>
2025-03-15 07:12:33 +0100

    StringBuilder Performance tests

---
+++ src/QuickTests/.NET/PerformanceTests/Benchmarks/DataTypes/Strings/StringBuilders/StringBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Globalization;
+using System.Text;
+
+namespace PerformanceTests.Benchmarks.DataTypes.Strings.StringBuilders;
+
+[MemoryDiagnoser]
+public class StringBuilderTests
+{
+    private readonly CultureInfo _culture = CultureInfo.InvariantCulture;
+    private const string DefaultText = "DefaultText";
+
+    [Params(Math.PI)]
+    public double Value;
+
+    [Benchmark] public string Append() => new StringBuilder(DefaultText).Append(Value).ToString();
+    [Benchmark] public string AppendToString() => new StringBuilder(DefaultText).Append(Value.ToString(_culture)).ToString();
+    [Benchmark] public string AppendFormat() => new StringBuilder(DefaultText).AppendFormat(_culture, "{0}", Value).ToString();
+    [Benchmark] public string AppendFormatInterpolated() => new StringBuilder(DefaultText).AppendFormat(_culture, $"{Value}").ToString();
+}
