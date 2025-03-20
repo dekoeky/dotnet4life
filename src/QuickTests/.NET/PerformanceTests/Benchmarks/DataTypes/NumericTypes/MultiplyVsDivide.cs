7e1abaa594245f28a32952289e5be03c52bb2c82
Wesley Van Koeckhoven <wvk@outlook.be>
2025-03-20 13:02:04 +0100

    Multiply & Division Performance comparison

---
+++ src/QuickTests/.NET/PerformanceTests/Benchmarks/DataTypes/NumericTypes/MultiplyVsDivide.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PerformanceTests.Benchmarks.DataTypes.NumericTypes;
+
+public class MultiplyVsDivide
+{
+    [Params(33.4)]
+    public double a;
+
+    [Params(11.1)]
+    public double b;
+
+    public double bInvers;
+
+    [GlobalSetup]
+    public void Setup() => bInvers = 1 / b;
+
+    [Benchmark] public double Divide() => a / b;
+    [Benchmark] public double Multiply() => a * bInvers;
+}
---
+++ src/QuickTests/.NET/PerformanceTests/Benchmarks/DataTypes/NumericTypes/MultiplyVsDivideConstants.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PerformanceTests.Benchmarks.DataTypes.NumericTypes;
+
+public class MultiplyVsDivideConstants
+{
+    private const double RadiansPerDegree = 2 * Math.PI / 360;
+    private const double DegreesPerRadian = 1 / RadiansPerDegree;
+
+    [Params(66.4)]
+    public double a;
+
+    [Benchmark] public double Divide() => a / DegreesPerRadian;
+    [Benchmark] public double Multiply() => a * RadiansPerDegree;
+}
