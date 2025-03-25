ec790f4a66741d676f0a6a4174159e299c6462fe
Wesley Van Koeckhoven <wvk@outlook.be>
2025-03-25 21:48:05 +0100

    Static interface methods invoking via reflection

---
+++ src/QuickTests/.NET/QuickTests/Reflection/StaticInterfaceMethodsViaReflection.cs
@@ -0,0 +1,143 @@
+﻿using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace QuickTests.Reflection;
+
+/// <summary>
+/// Demonstrates calling static interface methods, on implementing types (via reflection).
+/// </summary>
+[TestClass]
+public class StaticInterfaceMethodsViaReflection
+{
+    [TestMethod]
+    public void MethodC_StaticMethodOnType()
+    {
+        //Arrange
+        var type = typeof(TestType);
+        var method = type.GetMethod(nameof(TestType.MethodC),
+                         BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                     ?? throw new InvalidOperationException("Could not get method");
+
+        //Act
+        method.Invoke(null, null);
+
+        //Assert
+        Assert.IsTrue(TestType.Called);
+    }
+
+    [TestMethod]
+    public void MethodA_ViaImplicitInterfaceImplementation()
+    {
+        //Arrange
+        var type = typeof(TestType);
+        var method = type.GetMethod(nameof(TestType.MethodA),
+                         BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                     ?? throw new InvalidOperationException("Could not get method");
+
+        //Act
+        method.Invoke(null, null);
+
+        //Assert
+        Assert.IsTrue(TestType.Called);
+    }
+
+    [TestMethod]
+    public void MethodA_ViaExplicitInterfaceImplementation()
+    {
+        //Arrange
+        var type = typeof(TestType);
+        var interfaceMapping = type.GetInterfaceMap(typeof(IA));
+        var targetMethod = interfaceMapping.TargetMethods.First();
+
+        //Act
+        targetMethod.Invoke(null, null);
+
+        //Assert
+        Assert.IsTrue(TestType.Called);
+    }
+
+    [TestMethod]
+    public void MethodB_ViaExplicitInterfaceImplementation()
+    {
+        //Arrange
+        var type = typeof(TestType);
+        var interfaceMapping = type.GetInterfaceMap(typeof(IB));
+        var targetMethod = interfaceMapping.TargetMethods.First();
+
+        //Act
+        targetMethod.Invoke(null, null);
+
+        //Assert
+        Assert.IsTrue(TestType.Called);
+    }
+
+    [TestMethod]
+    public void PrintReflectionInfo()
+    {
+        //Arrange
+        using var w = new IndentedTextWriter(Console.Out);
+        var type = typeof(TestType);
+
+        //Act
+        w.WriteLine($"Type {type.Name} has the following:");
+        w.Indent++;
+        w.WriteHeaderWithItems("Methods:", type.GetMethods().Select(m => $"{m.Name}"));
+        w.WriteHeaderWithItems("Interfaces:", type.GetInterfaces().Select(i => $"{i.Name}"));
+    }
+
+    private class TestType : IA, IB
+    {
+        public static bool Called = false;
+
+
+        public static void MethodA()
+        {
+            Console.WriteLine($"Calling {nameof(MethodA)}, which is implicitly implemented on type {nameof(TestType)}");
+            Called = true;
+        }
+
+        static void IB.MethodB()
+        {
+            Console.WriteLine($"Calling {nameof(IB.MethodB)}, which is explicitly implemented on type {nameof(TestType)}");
+            Called = true;
+        }
+
+        public static void MethodC()
+        {
+            Console.WriteLine($"Calling {nameof(MethodC)}, which is a static method on type {nameof(TestType)}");
+            Called = true;
+        }
+    }
+
+    private interface IA
+    {
+        static abstract void MethodA();
+    }
+
+    private interface IB
+    {
+        static abstract void MethodB();
+    }
+}
+
+file static class IndentedTextWriterExtensions
+{
+    public static void WriteHeaderWithItems(this IndentedTextWriter tw, string title, IEnumerable<string> s)
+    {
+        using var e = s.GetEnumerator();
+
+        tw.Write(title);
+        if (!e.MoveNext())
+            return;
+        tw.WriteLine();
+
+        tw.Indent++;
+        do
+        {
+            tw.WriteLine(e.Current);
+        } while (e.MoveNext());
+
+        tw.Indent--;
+    }
+}
+
---
+++ src/QuickTests/.NET/QuickTests/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace QuickTests;
+
+public static class TypeExtensions
+{
+    public static string GetFriendlyTypeName(this Type type)
+    {
+        if (!type.IsGenericType)
+            return type.Name;
+
+        var typeName = type.Name.Split('`')[0]; // Remove arity suffix
+        var genericArgs = string.Join(", ", type.GetGenericArguments().Select(GetFriendlyTypeName));
+
+        return $"{typeName}<{genericArgs}>";
+    }
+}
