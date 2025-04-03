bfa83f609df8b8502b6aecec1ee7498db1c682bf
Wesley Van Koeckhoven <wvk@outlook.be>
2025-04-03 15:12:41 +0200

    enum flag check

--- src/QuickTests/.NET/PerformanceTests/Benchmarks/Enums/EnumFlagCheck.cs
+++ src/QuickTests/.NET/PerformanceTests/Benchmarks/Enums/EnumFlagCheck.cs
@@ -2,42 +2,31 @@
 
 namespace PerformanceTests.Benchmarks.Enums;
 
-[ShortRunJob]
-//[SimpleJob]
-//[Config(typeof())]
 public class EnumFlagCheck
 {
     [Benchmark]
     [ArgumentsSource(nameof(TestData))]
-    public bool Old(Status status, Status flagToCheck, bool _) => status.HasFlag(flagToCheck);
+    public bool HasFlag(Status status, Status flagToCheck) => status.HasFlag(flagToCheck);
 
     [Benchmark]
     [ArgumentsSource(nameof(TestData))]
-    public bool New(Status status, Status flagToCheck, bool _) => (status & flagToCheck) == flagToCheck;
-
-
+    public bool AndOperator(Status status, Status flagToCheck) => (status & flagToCheck) == flagToCheck;
 
 
     [Flags]
     public enum Status : byte
     {
-        PICTURE_ACTIVE = 0x01,
-        AUTODIMMING_ACTIVE = 0x02,
-        RESTARTED = 0x04,
-        COMMUNICATION_TIMEOUT = 0x08,
-        UPS_ACTIVE = 0x10,
-        MANUAL_CONTROL = 0x20,
-        CLIMATE_WARNING = 0x40,
-        BUSY = 0x80
+        Flag1 = 0b00000001,
+        Flag2 = 0b00000010,
+        Flag3 = 0b00000100
     }
 
     public static IEnumerable<object[]> TestData() =>
     [
-        // Status,  Flag to check,  expected result
-
-        [Status.BUSY, Status.BUSY, true],
-        [Status.BUSY, Status.CLIMATE_WARNING, false],
-        [Status.BUSY | Status.CLIMATE_WARNING, Status.BUSY, true ],
-        [Status.BUSY | Status.CLIMATE_WARNING, Status.CLIMATE_WARNING, true ],
+        // Status,  Flag to check
+        [Status.Flag2, Status.Flag2],
+        [Status.Flag2, Status.Flag1],
+        [Status.Flag2 | Status.Flag1, Status.Flag2 ],
+        [Status.Flag2 | Status.Flag1, Status.Flag1 ],
     ];
 }
