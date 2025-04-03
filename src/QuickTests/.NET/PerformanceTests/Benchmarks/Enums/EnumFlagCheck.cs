ecd239b623ab503e3d4157487e7218842feb27f2
Wesley Van Koeckhoven <wvk@outlook.be>
2025-04-03 21:01:23 +0200

    enum performance

--- src/QuickTests/.NET/PerformanceTests/Benchmarks/Enums/EnumFlagCheck.cs
+++ src/QuickTests/.NET/PerformanceTests/Benchmarks/Enums/EnumFlagCheck.cs
@@ -12,20 +12,23 @@ public class EnumFlagCheck
     [ArgumentsSource(nameof(TestData))]
     public bool AndOperator(Status status, Status flagToCheck) => (status & flagToCheck) == flagToCheck;
 
-
     [Flags]
     public enum Status : byte
     {
         Flag1 = 0b00000001,
         Flag2 = 0b00000010,
-        Flag3 = 0b00000100
     }
 
     public static IEnumerable<object[]> TestData() =>
     [
         // Status,  Flag to check
+        //--------------------------
+        [Status.Flag1, Status.Flag1],
         [Status.Flag2, Status.Flag2],
+
+        [Status.Flag1, Status.Flag2],
         [Status.Flag2, Status.Flag1],
+
         [Status.Flag2 | Status.Flag1, Status.Flag2 ],
         [Status.Flag2 | Status.Flag1, Status.Flag1 ],
     ];
