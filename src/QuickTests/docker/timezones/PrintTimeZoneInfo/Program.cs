e9891cb560c90f7a64b721f51d56dcec2abe5889
Wesley Van Koeckhoven <wvk@outlook.be>
2025-04-30 12:48:35 +0200

    printtimezone

---
+++ src/QuickTests/docker/timezones/PrintTimeZoneInfo/Program.cs
@@ -0,0 +1,11 @@
+﻿var tz = TimeZoneInfo.Local;
+var now = DateTime.Now;
+var utcNow = now.ToUniversalTime();
+var offset = now - utcNow;
+
+Console.WriteLine($"DisplayName:              {tz.DisplayName}");
+Console.WriteLine($"Daylight Savings Support: {tz.SupportsDaylightSavingTime}");
+Console.WriteLine($"Local Time:               {now}");
+Console.WriteLine($"UTC Time:                 {utcNow}");
+Console.WriteLine($"Offset:                   {offset}");
+Console.WriteLine();
