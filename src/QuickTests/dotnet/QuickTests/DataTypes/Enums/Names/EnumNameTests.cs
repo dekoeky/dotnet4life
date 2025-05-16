35668d041376d0614158e3c8721a4336dc48af9d
Wesley Van Koeckhoven <wvk@outlook.be>
2025-05-16 12:25:07 +0200

    EnumNameTests

---
+++ src/QuickTests/dotnet/QuickTests/DataTypes/Enums/Names/EnumNameTests.cs
@@ -0,0 +1,36 @@
+﻿namespace QuickTests.DataTypes.Enums.Names;
+
+[TestClass]
+public class EnumNameTests
+{
+    [DataTestMethod]
+    [DataRow(MyEnum.None, MyEnum.Empty)]
+    [DataRow(MyEnum.A, MyEnum.ValueA)]
+    [DataRow(MyEnum.B, MyEnum.ValueB)]
+    [DataRow(MyEnum.C, MyEnum.ValueC)]
+    public void AreEqual(MyEnum a, MyEnum b) => Assert.AreEqual(a, b);
+
+    [DataTestMethod]
+    [DataRow(MyEnum.ValueA, MyEnum.Valuea)]
+    public void AreNotEqual(MyEnum a, MyEnum b) => Assert.AreNotEqual(a, b);
+
+    [DataTestMethod]
+    [DataRow(nameof(MyEnum.None), MyEnum.None)]
+    [DataRow(nameof(MyEnum.Empty), MyEnum.None)]
+    [DataRow(nameof(MyEnum.ValueA), MyEnum.ValueA)]
+    [DataRow(nameof(MyEnum.ValueB), MyEnum.ValueB)]
+    [DataRow(nameof(MyEnum.ValueC), MyEnum.ValueC)]
+    [DataRow(nameof(MyEnum.A), MyEnum.ValueA)]
+    [DataRow(nameof(MyEnum.B), MyEnum.ValueB)]
+    [DataRow(nameof(MyEnum.C), MyEnum.ValueC)]
+    [DataRow(nameof(MyEnum.Valuea), MyEnum.Valuea)]
+    public void ParsesTo(string str, MyEnum expected)
+    {
+        // ---------- ACT --------------
+        var success = Enum.TryParse(str, out MyEnum result);
+
+        // ---------- ASSERT -----------
+        Assert.IsTrue(success);
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ src/QuickTests/dotnet/QuickTests/DataTypes/Enums/Names/MyEnum.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace QuickTests.DataTypes.Enums.Names;
+
+public enum MyEnum : byte
+{
+    None = 0,
+
+    ValueA = 1,
+    ValueB = 2,
+    ValueC = 3,
+
+    [SuppressMessage("ReSharper", "IdentifierTypo", Justification = "Demonstrates that enum values can have different casing")]
+    Valuea = 255,
+
+
+    A = ValueA,
+    B = ValueB,
+    C = ValueC,
+
+    //Alternative name for None
+    Empty = 0,
+}
