3c89aeda8d845b7954f775a1310b1c8299ac0a54
Wesley Van Koeckhoven <wvk@outlook.be>
2025-06-04 14:48:04 +0200

    keyed services

--- src/QuickTests/dotnet/QuickTests/DependencyInjection/KeyedServiceTests.cs
+++ src/QuickTests/dotnet/QuickTests/DependencyInjection/KeyedServiceTests.cs
@@ -6,7 +6,7 @@ namespace QuickTests.DependencyInjection;
 public class KeyedServiceTests
 {
     [TestMethod]
-    public void Test()
+    public void Test1()
     {
         //Arrange
         var services = new ServiceCollection();
@@ -41,6 +41,59 @@ public class KeyedServiceTests
         }
     }
 
+    [TestMethod]
+    public void Test3()
+    {
+        //Arrange
+        var services = new ServiceCollection();
+        services.AddKeyedSingleton<IData>("A", new Data("A"));
+        services.AddKeyedSingleton<IData>("B", new Data("B"));
+        services.AddSingleton<ServiceA>();
+        services.AddSingleton<ServiceB>();
+        using var serviceProvider = services.BuildServiceProvider();
+
+        //Act
+        var a = serviceProvider.GetService<ServiceA>();
+        var b = serviceProvider.GetService<ServiceB>();
+        var contentA = a?.GetContent();
+        var contentB = b?.GetContent();
+
+        //Assert
+        Assert.IsNotNull(a);
+        Assert.IsNotNull(b);
+        Assert.AreEqual("A", contentA);
+        Assert.AreEqual("B", contentB);
+    }
+
+    [TestMethod]
+    public void Test4()
+    {
+        //Arrange
+        byte key = 15;
+        var services = new ServiceCollection();
+        services.AddKeyedSingleton<IData>(key, new Data("Hello World"));
+        using var serviceProvider = services.BuildServiceProvider();
+
+        //Act
+        Assert.IsNotNull(serviceProvider.GetKeyedService<IData>(key));
+
+        //The following references are not equal to the original key
+        Assert.IsNull(serviceProvider.GetKeyedService<IData>((short)key));
+        Assert.IsNull(serviceProvider.GetKeyedService<IData>((int)key));
+        Assert.IsNull(serviceProvider.GetKeyedService<IData>((long)key));
+        Assert.IsNull(serviceProvider.GetKeyedService<IData>((ushort)key));
+        Assert.IsNull(serviceProvider.GetKeyedService<IData>((uint)key));
+        Assert.IsNull(serviceProvider.GetKeyedService<IData>((ulong)key));
+    }
+
+    private class ServiceA([FromKeyedServices("A")] IData data)
+    {
+        public string GetContent() => data.Content;
+    }
+    private class ServiceB([FromKeyedServices("B")] IData data)
+    {
+        public string GetContent() => data.Content;
+    }
     private record Data(string Content) : IData;
 
     private interface IData
