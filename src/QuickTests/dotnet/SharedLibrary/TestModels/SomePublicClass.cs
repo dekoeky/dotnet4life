ca8c90c5892f5f6bcd49198898751774aeab5fd4
Wesley Van Koeckhoven <wvk@outlook.be>
2025-06-05 10:17:57 +0200

    internals visible to tests

---
+++ src/QuickTests/dotnet/SharedLibrary/TestModels/SomePublicClass.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedLibrary.TestModels;
+
+public class SomePublicClass
+{
+    /// <summary>
+    /// An internal method that returns a greeting message.
+    /// </summary>
+    /// <remarks>
+    /// This method is marked as internal, in order to test the InternalsVisibleTo attribute functionality.
+    /// </remarks>
+    internal string SomeInternalMethod() => $"Hello from {nameof(SomePublicClass)}.{nameof(SomeInternalMethod)}!";
+}
