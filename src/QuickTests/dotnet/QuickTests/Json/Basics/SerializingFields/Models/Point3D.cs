c4f0353e001ff5c79a21872def352e0d0e9fa3e8
Wesley Van Koeckhoven <wvk@outlook.be>
2025-08-13 16:22:08 +0200

    Json Fields

---
+++ src/QuickTests/dotnet/QuickTests/Json/Basics/SerializingFields/Models/Point3D.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace QuickTests.Json.Basics.SerializingFields.Models;
+public struct Point3D
+{
+    [JsonPropertyName("X")] public double X;
+    [JsonPropertyName("Y")] public double Y;
+    [JsonPropertyName("Z")] public double Z;
+}
---
+++ src/QuickTests/dotnet/QuickTests/Json/Basics/SerializingFields/Models/Point3DWithJsonInclude.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace QuickTests.Json.Basics.SerializingFields.Models;
+
+public struct Point3DWithJsonInclude
+{
+    [JsonInclude][JsonPropertyName("X")] public double X;
+    [JsonInclude][JsonPropertyName("Y")] public double Y;
+    [JsonInclude][JsonPropertyName("Z")] public double Z;
+}
