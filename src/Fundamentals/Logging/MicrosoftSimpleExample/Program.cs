8044e5cff3a01e36f7f2603e86c9185b02bde87b
Wesley Van Koeckhoven <wvk@outlook.be>
2025-08-29 10:57:46 +0200

    logging

--- src/Fundamentals/Logging/MicrosoftSimpleExample/Program.cs
+++ src/Fundamentals/Logging/MicrosoftSimpleExample/Program.cs
@@ -1,14 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Logging;
 
-using Microsoft.Extensions.Logging;
-
-var loggerFactory = LoggerFactory.Create(builder =>
+//Logger factory defines where the logger will log to
+using var loggerFactory = LoggerFactory.Create(builder =>
 {
     builder.AddConsole();
     builder.AddDebug();
 });
 
+//Create a logger from the factory, using a class as category
 ILogger logger = loggerFactory.CreateLogger<Program>();
 
 logger.LogInformation("This is an info log.");
 logger.LogError("This is an error log.");
+
+
+ILogger breakfastLogger = loggerFactory.CreateLogger("breakfast");
+
+breakfastLogger.LogDebug("Toasting bread...");
+await Task.Delay(500);
+breakfastLogger.LogWarning("Toast is a bit burnt, oops");
+breakfastLogger.LogInformation("Toast ready!");
+breakfastLogger.LogDebug("Spreading jam over toast...");
+await Task.Delay(200);
+breakfastLogger.LogDebug("Pouring coffee...");
+await Task.Delay(300);
+breakfastLogger.LogInformation("Breakfast is ready!");
+breakfastLogger.LogCritical("OOPS! Dropped the plate!");
--- src/Fundamentals/Logging/SerilogSimpleExample/Program.cs
+++ src/Fundamentals/Logging/SerilogSimpleExample/Program.cs
@@ -1,10 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Serilog;
+﻿using Serilog;
 
 Console.WriteLine("Hello, World!");
 
 
 using var logger = new LoggerConfiguration()
-    .WriteTo.Console()
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss.fffffff}][{Level:u3}] ({SourceContext}) {Message:lj}{NewLine}{Exception}")
     .CreateLogger();
+
+
+
+var ferrariLogger = logger.ForContext<Mercedes>();
+var fiatLogger = logger.ForContext<Renault>();
+
+ferrariLogger.Information("Vroom Vroom");
+fiatLogger.Information("Tuut tuut");
+
+
+file class Mercedes;
+file class Renault;
