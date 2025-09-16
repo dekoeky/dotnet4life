f5803b7a7389a0a9f9017e62cd3094f9bfd1dbd4
Wesley Van Koeckhoven <wvk@outlook.be>
2025-09-16 13:57:05 +0200

    enum defaults test

---
+++ src/QuickTests/dotnet/QuickTests/DataTypes/Enums/Defaults/EnumDefaultsTests.cs
@@ -0,0 +1,38 @@
+﻿namespace QuickTests.DataTypes.Enums.Defaults;
+
+[TestClass]
+public class EnumDefaultsTests
+{
+    [TestMethod] public void EnumAbcDefaults() => Test<EnumAbc>();
+    [TestMethod] public void PosNegDefaults() => Test<PosNeg>();
+
+
+    private static void Test<T>() where T : struct, Enum
+    {
+        // ---------- ACT --------------
+        var defaultValue = default(T);
+        var defaultValueDefined = Enum.IsDefined(defaultValue);
+        var possibleValues = Enum.GetValues<T>()
+            .Select(EnumExtensions.NumericAndRegularValue)
+            .ToArray();
+
+        // ---------- ASSERT -----------
+        Console.WriteLine($"Default '{typeof(T).Name}' value: {defaultValue}");
+        Console.WriteLine($"Default '{typeof(T).Name}' value is defined: {defaultValueDefined}");
+        Console.WriteLine("Possible values:");
+        foreach (var possibleValue in possibleValues) Console.WriteLine($"    {possibleValue}");
+    }
+
+    private enum EnumAbc
+    {
+        A,
+        B,
+        C,
+    }
+
+    private enum PosNeg
+    {
+        Positive = +1,
+        Negative = -1,
+    }
+}
---
+++ src/QuickTests/dotnet/QuickTests/DataTypes/Enums/Defaults/EnumExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace QuickTests.DataTypes.Enums.Defaults;
+
+public static class EnumExtensions
+{
+    public static string NumericAndRegularValue<T>(this T value) where T : struct, Enum => $"[{Convert.ToInt32(value)}] {value}";
+}
