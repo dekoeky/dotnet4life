50665356b3c4e05aee62b4dcbce95e0e274fdae1
Wesley Van Koeckhoven <wvk@outlook.be>
2025-10-01 21:48:48 +0200

    cleanup

--- src/QuickTests/dotnet/QuickTests/_Helpers/RepeatTestMethodAttribute.cs
+++ src/QuickTests/dotnet/QuickTests/_Helpers/RepeatTestMethodAttribute.cs
@@ -1,24 +1,24 @@
 ﻿namespace QuickTests._Helpers;
 
 /// <summary>
-/// Repeats the annotated test method <see cref="RepeatCount"/> times.
+/// Repeats the annotated test method <see cref="_repeatCount"/> times.
 /// </summary>
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Method)]
 public sealed class RepeatTestMethodAttribute : TestMethodAttribute
 {
-    private readonly int RepeatCount;
+    private readonly int _repeatCount;
 
     public RepeatTestMethodAttribute(int repeatCount)
     {
         if (repeatCount < 1)
             throw new ArgumentOutOfRangeException(nameof(repeatCount), "Repeat count must be > 0");
-        RepeatCount = repeatCount;
+        _repeatCount = repeatCount;
     }
 
     public override TestResult[] Execute(ITestMethod testMethod)
     {
-        var results = new TestResult[RepeatCount];
-        for (var i = 0; i < RepeatCount; i++)
+        var results = new TestResult[_repeatCount];
+        for (var i = 0; i < _repeatCount; i++)
         {
             var result = base.Execute(testMethod);
             results[i] = result[0];
