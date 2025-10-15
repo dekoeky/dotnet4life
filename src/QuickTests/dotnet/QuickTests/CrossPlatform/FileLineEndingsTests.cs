ce7e5bcc256faad98de67c142a0b65b7a0c69133
Wesley Van Koeckhoven <wvk@outlook.be>
2025-10-16 00:14:08 +0200

    CrossPlatform FileLineEndings test

---
+++ src/QuickTests/dotnet/QuickTests/CrossPlatform/FileLineEndingsTests.cs
@@ -0,0 +1,46 @@
+﻿using QuickTests.CrossPlatform.TestFiles;
+
+namespace QuickTests.CrossPlatform;
+
+[TestClass]
+public class FileLineEndingsTests
+{
+    [TestMethod]
+    [DataRow(TestData.CrLfFilePath, TestData.CrLfFileContent)]
+    [DataRow(TestData.LfFilePath, TestData.LfFileContent)]
+    public void ValidateTestFileContents(string path, string expectedContent)
+    {
+        // ---------- ACT --------------
+        var content = File.ReadAllText(path);
+
+        // ---------- ASSERT -----------
+        Assert.AreEqual(expectedContent, content, StringComparer.Ordinal);
+    }
+
+    [TestMethod]
+    [DataRow(TestData.CrLfFilePath)]
+    [DataRow(TestData.LfFilePath)]
+    public void ReadAllLines(string path)
+    {
+        // ---------- ACT --------------
+        var lines = File.ReadAllLines(path);
+
+        // ---------- ASSERT -----------
+        CollectionAssert.AreEqual(TestData.ExpectedLines, lines, StringComparer.Ordinal);
+    }
+
+    /// <summary>
+    /// Demonstrates that both CRLF and LF line endings are correctly handled by <see cref="File.ReadLines(string)"/> on both linux and Windows.
+    /// </summary>
+    [TestMethod]
+    [DataRow(TestData.CrLfFilePath)]
+    [DataRow(TestData.LfFilePath)]
+    public void ReadLines(string path)
+    {
+        // ---------- ACT --------------
+        var lines = File.ReadAllLines(path);
+
+        // ---------- ASSERT -----------
+        CollectionAssert.AreEqual(TestData.ExpectedLines, lines, StringComparer.Ordinal);
+    }
+}
