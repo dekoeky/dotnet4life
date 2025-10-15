5bedd5689aab0bc115fe5810577f6ace5174eba6
Wesley Van Koeckhoven <wvk@outlook.be>
2025-10-15 23:34:01 +0200

    cleanup

--- src/QuickTests/dotnet/QuickTests/MultiThreading/ConcurrentBagTests.cs
+++ src/QuickTests/dotnet/QuickTests/MultiThreading/ConcurrentBagTests.cs
@@ -5,19 +5,21 @@ namespace QuickTests.MultiThreading;
 [TestClass]
 public class ConcurrentBagTests
 {
+    private readonly int producers = Environment.ProcessorCount * 4;     // Make sure there are enough producers simultaneously trying to access the list to make the problem more likely.
+    private const int PerProducer = 100_000;
+
     [TestMethod]
     public void List_is_not_thread_safe_for_writes()
     {
         //ARRANGE
         var bag = new List<int>();
-        int producers = Environment.ProcessorCount * 4;     // Make sure there are enough producers simultaneously trying to access the list to make the problem more likely.
-        int perProducer = 100_000;
-        int droppedDueToException = 0;
+
+        var droppedDueToException = 0;
 
         //ACT
         Parallel.For(0, producers, _ =>
         {
-            for (var i = 0; i < perProducer; i++)
+            for (var i = 0; i < PerProducer; i++)
                 try
                 {
                     bag.Add(i); // data race
@@ -31,7 +33,7 @@ public class ConcurrentBagTests
                 }
         });
 
-        int expected = producers * perProducer;                     // If a List<T> was thread-safe for writes, this is what we would expect.
+        var expected = producers * PerProducer;                     // If a List<T> was thread-safe for writes, this is what we would expect.
         var actual = bag.Count;                                     // How many items were actually added.
         var dropped = expected - actual;                            // How many items were dropped in total.
         var droppedDueToRace = dropped - droppedDueToException;     // How many items were dropped due to the race condition
@@ -53,28 +55,22 @@ public class ConcurrentBagTests
     {
         //ARRANGE
         var bag = new ConcurrentBag<int>();
-        int producers = Environment.ProcessorCount * 4;
-        int perProducer = 100_000;
-        int droppedDueToException = 0;
 
         //ACT
         Parallel.For(0, producers, _ =>
         {
-            for (var i = 0; i < perProducer; i++)
-                bag.Add(i); // data race
+            for (var i = 0; i < PerProducer; i++)
+                bag.Add(i);
         });
 
-        int expected = producers * perProducer;                     // If a List<T> was thread-safe for writes, this is what we would expect.
-        var actual = bag.Count;                                     // How many items were actually added.
-        var dropped = expected - actual;                            // How many items were dropped in total.
-        var droppedDueToRace = dropped - droppedDueToException;     // How many items were dropped due to the race condition
+        var expected = producers * PerProducer;
+        var actual = bag.Count;
+        var dropped = expected - actual;
 
         //Print results
         Console.WriteLine($"Expected Item Count:  {expected}");
         Console.WriteLine($"Actual Item Count:    {actual}");
         Console.WriteLine($"Dropped Item Count:   {dropped}");
-        Console.WriteLine($"Dropped by Exception: {droppedDueToException}");
-        Console.WriteLine($"Dropped by Race:      {droppedDueToRace}");
 
         //ASSERT
         Assert.AreEqual(expected, actual, "We expected ConcurrentBag.Add to lose items due to race condition.");
