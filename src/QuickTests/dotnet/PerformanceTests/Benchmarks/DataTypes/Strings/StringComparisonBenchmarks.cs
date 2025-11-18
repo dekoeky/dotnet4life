7312de4a2103c3a80a344c89bf0d5d2b5eefd649
Wesley Van Koeckhoven <wvk@outlook.be>
2025-11-18 11:29:26 +0100

    StringComparison tests

---
+++ src/QuickTests/dotnet/PerformanceTests/Benchmarks/DataTypes/Strings/StringComparisonBenchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+// ReSharper disable UnassignedField.Global
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+namespace PerformanceTests.Benchmarks.DataTypes.Strings;
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class StringComparisonBenchmarks
+{
+    private const string TestValue = "Hello World";
+
+    [Params(null, "", TestValue)]
+    public string? NullableString;
+
+    [Params(TestValue)]
+    public string NonNullableString;
+
+
+    [Benchmark]
+    public bool NullCheck_And_Nullable_Equals_NonNullable() => NullableString is not null && NullableString.Equals(NonNullableString);
+
+    [Benchmark]
+    public bool NonNullable_Equals_Nullable() => NonNullableString.Equals(NullableString);
+
+    [Benchmark]
+    public bool EqualityOperator() => NonNullableString == NullableString;
+}
---
+++ src/QuickTests/dotnet/QuickTests/DataTypes/Strings/StringComparisonTests.cs
@@ -0,0 +1,69 @@
+﻿namespace QuickTests.DataTypes.Strings;
+
+[TestClass]
+public class StringComparisonTests
+{
+    [TestMethod]
+    [DataRow("Hello World", "Hello World", StringComparison.CurrentCulture, true)]
+    public void SimpleExample(string a, string b, StringComparison comparison, bool expectedEqual)
+    {
+        //Arrange
+        var comparer = StringComparer.FromComparison(comparison);
+
+        //Act
+        var actuallyEqual = comparer.Equals(a, b);
+
+        //Assert
+        Assert.AreEqual(expectedEqual, actuallyEqual);
+    }
+
+    /// <summary>
+    /// Demonstrate that calling <see cref="string.Equals(string?)"/>
+    /// on a null string throws a <see cref="NullReferenceException"/>.
+    /// </summary>
+    /// <remarks><see cref="EqualityOperatorOnNullStringDoesNotThrow"/> for an alternative solution.</remarks>
+    [TestMethod]
+    public void EqualsOnNullStringThrowsNullReferenceException()
+    {
+        //Arrange
+        const string? a = null;
+        const string b = "SomePredeterminedValue";
+        const bool expectedEqual = false;
+
+        //Act
+        Assert.Throws<NullReferenceException>(() =>
+        {
+            // Dereference of a possibly null reference.
+            // More Info: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/nullable-warnings#possible-dereference-of-null
+#pragma warning disable CS8602 
+            var actual = a.Equals(b); // No StringComparison used here for simplicity
+#pragma warning restore CS8602
+
+            //Assert
+            Assert.AreEqual(expectedEqual, actual);
+        });
+
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(NullableStringVsNonNullableStringTestData))]
+    public void EqualityOperatorOnNullStringDoesNotThrow(string? a, string b, StringComparison comparison, bool expectedEqual)
+    {
+        //Act
+        var actuallyEqual = a == b; // The equality operator is overloaded to hangle null values gracefully.
+
+        //Assert
+        Assert.AreEqual(expectedEqual, actuallyEqual);
+    }
+
+    public static IEnumerable<object?[]> NullableStringVsNonNullableStringTestData()
+    {
+        foreach (var comparison in Enum.GetValues<StringComparison>())
+        {
+            // Test cases should return the same result for each string comparison
+            yield return [null, "Hello World", comparison, false];
+            yield return ["", "Hello World", comparison, false];
+            yield return ["Hello World", "Hello World", comparison, true];
+        }
+    }
+}
