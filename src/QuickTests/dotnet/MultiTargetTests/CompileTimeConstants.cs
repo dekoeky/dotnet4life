c831fa9a3d19366794843552195ec30ccc6f02f5
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-05 17:11:17 +0100

    include target framework & bits in output

---
+++ src/QuickTests/dotnet/MultiTargetTests/CompileTimeConstants.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MultiTargetTests;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+internal static class CompileTimeConstants
+{
+#if NET47
+    public const string TFM = "net47";
+#elif NET471
+    public const string TFM = "net471";
+#elif NET472
+    public const string TFM = "net472";
+#elif NET48
+    public const string TFM = "net48";
+#elif NET481
+    public const string TFM = "net481";
+#elif NET8_0
+    public const string TFM = "net8.0";
+#elif NET9_0
+    public const string TFM = "net9.0";
+#else
+#error Could not detect TFM (Not Specified in CompileTimeConstants)
+#endif
+}
--- src/QuickTests/dotnet/MultiTargetTests/FloatTests.cs
+++ src/QuickTests/dotnet/MultiTargetTests/FloatTests.cs
@@ -6,7 +6,7 @@ namespace MultiTargetTests;
 public sealed class FloatTests
 {
     [TestMethod]
-    [DataRow(1.2345679f)]
+    [DataRow(1.2345679f)] // This value has (small) different results between for example .NET9 and .NET FrameWork 480
     [DataRow(0.1f)]
     public void ToString(float value)
     {
@@ -15,14 +15,47 @@ public sealed class FloatTests
         IFormatProvider formatProvider = CultureInfo.InvariantCulture;
 
         // ---------- ACT --------------
-        var results = formats.ToDictionary(f => f ?? "(null)", f => value.ToString(f, formatProvider));
+        var results = formats.ToDictionary(f => f ?? "null", f => value.ToString(f, formatProvider));
+        results.Add("bits", BitsOf(value).PadLeft(32, '0'));
 
         // ---------- ASSERT -----------
         foreach (var kv in results)
         {
             var format = kv.Key;
             var s = kv.Value;
-            Console.WriteLine($"[{format,6}] {s}");
+            Console.WriteLine($"[{CompileTimeConstants.TFM,7}] ({format,6}) {s}");
         }
+
+        // ---------------------------
+        // Example Outputs
+        // ---------------------------
+
+        // [ net481] (  null) 1.234568
+        // [ net481] (     G) 1.234568
+        // [ net481] (    G7) 1.234568
+        // [ net481] (    G9) 1.23456788
+        // [ net481] (     R) 1.23456788
+        // [ net481] (  bits) 11111111110011110000001100101001000000000000000000000000000000
+
+        // [ net9.0] (  null) 1.2345679
+        // [ net9.0] (     G) 1.2345679
+        // [ net9.0] (    G7) 1.234568
+        // [ net9.0] (    G9) 1.23456788
+        // [ net9.0] (     R) 1.2345679
+        // [ net9.0] (  bits) 11111111110011110000001100101001000000000000000000000000000000
+
+        // Conclusion:
+        //      Bits are always equal
+        //      null, G, G9, R interpretations are different
+        //      G7 interpretation is equal
+    }
+
+    /// <summary>
+    /// A (framework-independent) conversion of a float to a (32-)bit string.
+    /// </summary>
+    private static string BitsOf(float value)
+    {
+        var bits = BitConverter.DoubleToInt64Bits(value);
+        return Convert.ToString(bits, 2).PadLeft(32, '0');
     }
 }
