30b56a97776aee152ab49ec914bcfb007dce7161
Wesley Van Koeckhoven <wvk@outlook.be>
2026-03-05 18:28:25 +0100

    Fix CA1840

--- src/QuickTests/dotnet/QuickTests/Threading/LockTests.cs
+++ src/QuickTests/dotnet/QuickTests/Threading/LockTests.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using static System.Threading.Thread;
 
 namespace QuickTests.Threading;
 
@@ -9,7 +8,7 @@ public class LockTests
     [TestMethod]
     public async Task TestMethod1()
     {
-        Debug.WriteLine($"Unit test starting on thread {CurrentThread.ManagedThreadId}");
+        Debug.WriteLine($"Unit test starting on thread {Environment.CurrentManagedThreadId}");
         //Arrange
         var task1 = Task.Run(() => LockableMethod(TimeSpan.FromSeconds(2), "Task 1"));
         var task2 = Task.Run(() => LockableMethod(TimeSpan.FromSeconds(2), "Task 2"));
@@ -17,26 +16,26 @@ public class LockTests
         var tasks = new[] { task1, task2 };
 
         await Task.WhenAll(tasks);
-        Debug.WriteLine($"Unit test finished on thread {CurrentThread.ManagedThreadId}");
+        Debug.WriteLine($"Unit test finished on thread {Environment.CurrentManagedThreadId}");
     }
 
     private readonly SemaphoreSlim _semaphore = new(1, 1);
 
     private async Task LockableMethod(TimeSpan delay, string taskName)
     {
-        Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} entered method, on thread {CurrentThread.ManagedThreadId}");
+        Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} entered method, on thread {Environment.CurrentManagedThreadId}");
 
         await _semaphore.WaitAsync();
-        Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} acquired semaphore, on thread {CurrentThread.ManagedThreadId}");
+        Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} acquired semaphore, on thread {Environment.CurrentManagedThreadId}");
 
         try
         {
 
-            Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} entered semaphore, on thread {CurrentThread.ManagedThreadId}");
+            Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} entered semaphore, on thread {Environment.CurrentManagedThreadId}");
 
             await Task.Delay(delay);
 
-            Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} will exit semaphore, on thread {CurrentThread.ManagedThreadId}");
+            Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} will exit semaphore, on thread {Environment.CurrentManagedThreadId}");
         }
         finally
         {
@@ -44,6 +43,6 @@ public class LockTests
             _semaphore.Release();
         }
 
-        Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} finished method, on thread {CurrentThread.ManagedThreadId}");
+        Debug.WriteLine($"{DateTime.Now.TimeOfDay} {taskName} finished method, on thread {Environment.CurrentManagedThreadId}");
     }
 }
